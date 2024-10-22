af24dbd23d51bae161c627626088ad037a425b5a
vtalix-anshul <kumar.anshul@vtalix.in>
2024-10-22 12:56:54 +0530

    check kr le ho gya kya shi baaki ka tu dekh lena

--- suv/src/component/page/SingleProduct.jsx
+++ suv/src/component/page/SingleProduct.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
 import { getallsingleproduct } from '../store/Slices/SingleProductSlice'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-export default function () {
-  const {id} = useParams()
+export default function SingleProduct() {
+  const {id} = useParams();
   // console.log('params id',id)
-  const API = 'https://api.pujakaitem.com/api/products'
+  const API = 'https://api.pujakaitem.com/api/products';
 
-  const {fetchsingleproduct} = useSelector(state=> state.singleProducts)
-  console.log("single product data is",fetchsingleproduct)
+  const {fetchsingleproduct} = useSelector(state=> state.singleproduct);
+  // yahan pe state.singleproduct change kiya h maine.
+  // variables ke naam thk se use kiya kr.
+  // variables ke naamo ki maa chudi pdi h.
+  // state.singleproduct h vo, pta ni kya use krra tha chutiya.
+
+  
+  
+  // const fetchsingleproduct = useSelector(state=> console.log("state is ", state));
+  console.log("single product data is",fetchsingleproduct);
   useEffect(()=>{
-    getallsingleproduct(`${API}?id=${id}`)
-  },[])
+    getallsingleproduct(`${API}?id=${id}`);
+  },[]);
+
   return (
     <div>single product page</div>
   )
